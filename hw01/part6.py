d693b40f07cd04773b48f65e2bb6f16b00a489ef
alkarps <alserkarpov@gmail.com>
2022-02-13 10:25:26 +0300

    6 пункт

---
+++ hw01/part6.py
@@ -0,0 +1,14 @@
+income = int(input("Please, input income: "))
+outcome = int(input("Please, input outcome: "))
+if income < outcome:
+    print("You loose money")
+elif income == outcome:
+    print("Its equals")
+else:
+    print("You get money")
+    clear_income = income - outcome
+    rental_income = clear_income / income
+    print(f"rental income: {rental_income}")
+    personal_count = int(input("Please, input personal count: "))
+    rental_income_on_one_personal = rental_income / personal_count
+    print(f"rental income on one personal: {rental_income_on_one_personal}")
