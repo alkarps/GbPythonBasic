f3f5f39c0c0ead7c83ddd2b2d4e56b782eb0f5b3
alkarps <alserkarpov@gmail.com>
2022-02-14 22:38:22 +0300

    Добавлено новое решение 4го пункта

--- hw01/part4.py
+++ hw01/part4.py
@@ -10,3 +10,13 @@ while i < n:
         maxChar = current
     i += 1
 print(maxChar)
+maxChar = 0
+number = int(number)
+while number > 0:
+    current = number % 10
+    if current > maxChar:
+        maxChar = current
+        if maxChar == 9:
+            break
+    number //= 10
+print(maxChar)
