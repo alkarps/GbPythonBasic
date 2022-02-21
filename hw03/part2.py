08994f03d1c627403dbfe6acacc69fecaec56cc7
alkarps <alserkarpov@gmail.com>
2022-02-21 21:58:32 +0300

    fix 2 пункт

--- hw03/part2.py
+++ hw03/part2.py
@@ -1,6 +1,5 @@
 def print_personal(first_name, second_name, birthday, city, email, phone):
-    print(
-        f"Имя: {first_name}, фамилия: {second_name}, год рождения: {birthday}, город проживания: {city}, email: {email}, телефон: {phone}")
+    return f"Имя: {first_name}, фамилия: {second_name}, год рождения: {birthday}, город проживания: {city}, email: {email}, телефон: {phone}"
 
 
 p_fname = input("Введите имя: ")
@@ -9,4 +8,4 @@ p_birthday = input("Введите дату рождения: ")
 p_city = input("Введите город: ")
 p_email = input("Введите email: ")
 p_phone = input("Введите телефон: ")
-print_personal(first_name=p_fname, second_name=p_sname, birthday=p_birthday, city=p_city, email=p_email, phone=p_phone)
+print(print_personal(first_name=p_fname, second_name=p_sname, birthday=p_birthday, city=p_city, email=p_email, phone=p_phone))
