da1a3a93e369a208772e1d952636dee66a2b5005
alkarps <alserkarpov@gmail.com>
2022-02-28 21:35:44 +0300

    fix 2 пункт

--- hw05/part2.py
+++ hw05/part2.py
@@ -2,4 +2,4 @@ with open("part2.txt", "r") as file:
     lines = file.readlines()
     print(f"Count lines: {len(lines)}.")
     for ind, line in enumerate(lines):
-        print(f"{ind}'s line contain {len(line.split())} words.")
+        print(f"{ind + 1}'s line contain {len(line.split())} words.")
