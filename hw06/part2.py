a0e94a92475c60df79529f01f185492cffbae8cf
alkarps <alserkarpov@gmail.com>
2022-03-05 21:36:28 +0300

    fix 2 пункт

--- hw06/part2.py
+++ hw06/part2.py
@@ -1,12 +1,10 @@
 class Road:
-    __weight = 25
-    __height = 5
 
     def __init__(self, length, width):
         self.length, self.width = length, width
 
-    def get_weight(self):
-        return self.length * self.width * self.__weight * self.__height
+    def get_weight(self, weight, height):
+        return self.length * self.width * weight * height
 
 
-print(Road(20, 5000).get_weight())
+print(Road(20, 5000).get_weight(25, 5))
